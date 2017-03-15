beab0756c82cc2983eb8349f926c4af266f51e8d
joannahmazing <joannahrabilas@gmail.com>
2017-03-15 18:14:17 +0800

    minor

--- src/TextProcess/WordNetAccess.java
+++ src/TextProcess/WordNetAccess.java
@@ -33,12 +33,8 @@ public class WordNetAccess {
     public static IDictionary loadDic(){
         
         IDictionary retDictionary = null;
-        
-<<<<<<< HEAD
+
         String WNHome = "C:\\Users\\csc1701\\Google Drive\\Current Thesis\\tools";
-=======
-        String WNHome = "C:\\Users\\AnnTherese\\Google Drive\\Current Thesis\\tools\\WordNet-3.0";
->>>>>>> refs/remotes/origin/master
         String path = WNHome + File.separator + "dict";
         
         try{
